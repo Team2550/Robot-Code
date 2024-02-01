3b3bbbd4ed10c52a310c6d6f4b50d0ca2706ee51
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2024-02-01 15:36:03 -0800

    Implemet squared inputs.

--- src/main/cpp/subsystems/DriveSubsystem.cpp
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -13,12 +13,19 @@ DriveSubsystem::DriveSubsystem()
 /*
  * Drive functions
  */
-void DriveSubsystem::ArcadeDrive(double speed, double rotation) {
+void DriveSubsystem::ArcadeDrive(double speed, double rotation, bool squareInputs) {
+	speed = std::clamp(speed, -1.0, 1.0);
+	rotation = std::clamp(rotation, -1.0, 1.0);
 	// Deadzone
 	if (abs(speed) < OIConstants::kDeadzone)
 		speed = 0;
 	if (abs(rotation) < OIConstants::kDeadzone)
 		rotation = 0;
+
+	if (squareInputs) {
+		speed = std::copysign(speed * speed, speed);
+		rotation = std::copysign(rotation * rotation, rotation);
+	}
 
 	m_frontRight.Set(frontRightFilter.Calculate(speed + rotation));
 	m_rearRight.Set(rearRightFilter.Calculate(speed + rotation));
@@ -26,12 +33,19 @@ void DriveSubsystem::ArcadeDrive(double speed, double rotation) {
 	m_rearLeft.Set(rearLeftFilter.Calculate(speed - rotation));
 }
 
-void DriveSubsystem::TankDrive(double leftSpeed, double rightSpeed) {
+void DriveSubsystem::TankDrive(double leftSpeed, double rightSpeed, bool squareInputs) {
+	leftSpeed = std::clamp(leftSpeed, -1.0, 1.0);
+	rightSpeed = std::clamp(rightSpeed, -1.0, 1.0);
 	// Deadzone
 	if (abs(leftSpeed) < OIConstants::kDeadzone)
 		leftSpeed = 0;
 	if (abs(rightSpeed) < OIConstants::kDeadzone)
 		rightSpeed = 0;
+
+	if (squareInputs) {
+		leftSpeed = std::copysign(leftSpeed * leftSpeed, leftSpeed);
+		rightSpeed = std::copysign(rightSpeed * rightSpeed, rightSpeed);
+	}
 
 	m_frontRight.Set(frontRightFilter.Calculate(rightSpeed));
 	m_rearRight.Set(rearRightFilter.Calculate(rightSpeed));
