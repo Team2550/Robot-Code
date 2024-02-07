47b01d108e7eaab84d840930c18d98a1dfe7a78e
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2024-02-06 16:28:55 -0800

    Fix speed for pivot
    Remap controls for intake/expell

--- src/main/cpp/commands/TeleIntake.cpp
+++ src/main/cpp/commands/TeleIntake.cpp
@@ -9,14 +9,14 @@ TeleIntake::TeleIntake(IntakeSubsystem* subsystem, frc::XboxController* controll
 void TeleIntake::Initialize() { }
 
 void TeleIntake::Execute() {
-	if (m_controller->GetXButton()) {
+	if (m_controller->GetBButton()) {
 		m_intake->IntakeRing();
-	} else if (m_controller->GetYButton()) {
+	} else if (m_controller->GetAButton()) {
 		m_intake->ExpellRing();
 	} else {
 		m_intake->StopInake();
 	}
-	m_intake->PivotIntake(m_controller->GetLeftTriggerAxis(), m_controller->GetRightTriggerAxis());
+	m_intake->PivotIntake((0.5 * m_controller->GetLeftTriggerAxis()), (0.75 * m_controller->GetRightTriggerAxis()));
 }
 
 void TeleIntake::End(bool interrupted) { }
--- src/main/cpp/subsystems/IntakeSubsystem.cpp
+++ src/main/cpp/subsystems/IntakeSubsystem.cpp
@@ -16,6 +16,5 @@ void IntakeSubsystem::ExpellRing() { m_intakeMotor.Set(-1); }
 void IntakeSubsystem::StopInake() { m_intakeMotor.Set(0); }
 
 void IntakeSubsystem::PivotIntake(double upSpeed, double downSpeed) {
-	m_intakePivot.Set(
-		ctre::phoenix::motorcontrol::TalonSRXControlMode::PercentOutput, ((0.5 * upSpeed) - (0.5 * downSpeed)));
+	m_intakePivot.Set(ctre::phoenix::motorcontrol::TalonSRXControlMode::PercentOutput, (upSpeed - downSpeed));
 }
