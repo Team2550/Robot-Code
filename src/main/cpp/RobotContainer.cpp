c9f3e350f200101e48dd942f98e4a353a1f3f445
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2024-02-24 11:45:25 -0800

    Auto Adjustments.

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -18,33 +18,20 @@ void RobotContainer::ConfigureBindings() { }
 // For these autos it is assumed the robot is starting aligned with the speaker
 // Thus, Fire ring, turn to face line (if needed), leave starting zone.
 frc2::CommandPtr RobotContainer::GetLeftAutonomous() {
-	return frc2::cmd::Sequence(
-		AutoLaunch(&m_launch, &m_intake, 30).ToPtr(),
-		frc2::cmd::Wait(0.5_s),
-		AutoTurn(&m_drive, false, 15, ds).ToPtr(),
-		AutoDrive(&m_drive, false, 25).ToPtr());
+	return frc2::cmd::Sequence(AutoLaunch(&m_launch, &m_intake, 30).ToPtr(), frc2::cmd::Wait(0.5_s),
+		AutoTurn(&m_drive, false, 15, ds).ToPtr(), AutoDrive(&m_drive, false, 25).ToPtr());
 };
 frc2::CommandPtr RobotContainer::GetMiddleAutonomous() {
-	return frc2::cmd::Sequence(
-		AutoLaunch(&m_launch, &m_intake, 30).ToPtr(),
-		frc2::cmd::Wait(0.5_s),
-		AutoDrive(&m_drive, false, 25).ToPtr(),
-		AutoPivot(&m_intake, false, 10).ToPtr(),
-		AutoPickup(&m_intake, &m_drive, 30).ToPtr(),
-		frc2::cmd::Wait(0.5_s),
-		AutoPivot(&m_intake, true, 10).ToPtr(),
-		AutoDrive(&m_drive, false, 30).ToPtr(),
-		frc2::cmd::Wait(0.5_s),
-		AutoLaunch(&m_launch, &m_intake, 30).ToPtr(),
-		frc2::cmd::Wait(0.5_s),
-		AutoDrive(&m_drive, true, 20).ToPtr());
+	return frc2::cmd::Sequence(AutoLaunch(&m_launch, &m_intake, 30).ToPtr(), frc2::cmd::Wait(0.5_s),
+		AutoDrive(&m_drive, false, 25).ToPtr(), frc2::cmd::Wait(0.5_s), AutoPivot(&m_intake, false, 35).ToPtr(),
+		frc2::cmd::Wait(0.5_s), AutoPickup(&m_intake, &m_drive, 30).ToPtr(), frc2::cmd::Wait(0.5_s),
+		AutoPivot(&m_intake, true, 35).ToPtr(), frc2::cmd::Wait(0.5_s), AutoDrive(&m_drive, true, 30).ToPtr(),
+		frc2::cmd::Wait(0.5_s), AutoLaunch(&m_launch, &m_intake, 30).ToPtr(), frc2::cmd::Wait(0.5_s),
+		AutoDrive(&m_drive, false, 25).ToPtr());
 };
 frc2::CommandPtr RobotContainer::GetRightAutonomous() {
-	return frc2::cmd::Sequence(
-		AutoLaunch(&m_launch, &m_intake, 30).ToPtr(),
-		frc2::cmd::Wait(0.5_s),
-		AutoTurn(&m_drive, true, 15, ds).ToPtr(),
-		AutoDrive(&m_drive, false, 25).ToPtr());
+	return frc2::cmd::Sequence(AutoLaunch(&m_launch, &m_intake, 30).ToPtr(), frc2::cmd::Wait(0.5_s),
+		AutoTurn(&m_drive, true, 15, ds).ToPtr(), AutoDrive(&m_drive, false, 25).ToPtr());
 };
 
 frc2::CommandPtr RobotContainer::GetTestCommand() {};
--- src/main/cpp/commands/AutoPickup.cpp
+++ src/main/cpp/commands/AutoPickup.cpp
@@ -2,23 +2,23 @@
 
 AutoPickup::AutoPickup(IntakeSubsystem* intakeSubsystem, DriveSubsystem* driveSubsystem, int amount)
 	: m_drive(driveSubsystem)
-    , m_intake(intakeSubsystem)
+	, m_intake(intakeSubsystem)
 	, m_amount(amount) {
 	AddRequirements({ intakeSubsystem });
-    AddRequirements({ driveSubsystem });
+	AddRequirements({ driveSubsystem });
 }
 
-void AutoPickup::Initialize() {}
+void AutoPickup::Initialize() { }
 
 void AutoPickup::Execute() {
-    m_intake->IntakeRing();
-	m_drive->ArcadeDrive(0.5, 0, true);
-    m_amount = m_amount - 1;
+	m_intake->IntakeRing();
+	m_drive->ArcadeDrive(-0.5, 0, true);
+	m_amount = m_amount - 1;
 }
 
 void AutoPickup::End(bool interrupted) {
-    m_intake->StopIntake();
-    m_drive->ArcadeDrive(0,0, true);
+	m_intake->StopIntake();
+	m_drive->ArcadeDrive(0, 0, true);
 }
 
 bool AutoPickup::IsFinished() { return m_amount <= 0; }
